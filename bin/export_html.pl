298133bcd7779a0d89614cb5b6c4a88a203a979f
Anton Ukolov <aukolov@aukolov.ru>
2012-10-04 15:50:01 +0400

    added prettifying of code snippets

--- bin/export_html.pl
+++ bin/export_html.pl
@@ -4,18 +4,20 @@ use Modern::Perl;
 use Text::Markdown;
 
 ## usage: 
-## script *.md
+## bin/export_html.pl *.md
 
-my $header = q[
+my $header = qq#
 <html>
 <head>
 <title>Plack Handbook</title>
 <meta name="Author" content="Tatsuhiko Miyagawa">
 <meta name="DC.date.publication" content="2012-09">
 <meta name="DC.rights" content="(c) 2009-2012 Tatsuhiko Miyagawa">
+<link href="images/prettify.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="images/prettify.js"></script>
 </head>
 <body>
-];
+#;
 
 my $mt      = Text::Markdown->new;
 my $book_fh = IO::File->new("book.html", ">")
@@ -27,11 +29,25 @@ my $contents = join "\n", map {
     <$fh>;
 } @ARGV;
 
-my $html = $mt->markdown($contents);
-$html =~ s/<h2>/<h2 class="chapter">/g;
+my $html   = prepare_html($mt->markdown($contents));
+my $footer = qq#
+    <script>
+        document.body.onload = function () { prettyPrint() };
+    </script>
+    </body></html>
+#;
 
 $book_fh->say(
     $header,
-    $html, 
-    "</body></html>"
+    $html,
+    $footer,
 );
+
+sub prepare_html {
+    my $html = shift;
+
+    $html =~ s/<h2>/<h2 class="chapter">/g;
+    $html =~ s/<code>/<code class="prettyprint perl">/g;
+
+    return $html;
+}
